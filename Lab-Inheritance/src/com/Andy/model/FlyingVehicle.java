f312eeaf9b80e91ec8d27f873e4b4be09c313798
Andrew Lachin <andrewlachin@Andrews-MBP.attlocal.net>
2021-07-07 16:26:31 -0500

    Labs for Jul 7

---
+++ Lab-Inheritance/src/com/Andy/model/FlyingVehicle.java
@@ -0,0 +1,39 @@
+package com.Andy.model;
+
+public class FlyingVehicle extends Vehicle{
+	private short wings;
+	private Engine engine;
+	
+	public FlyingVehicle() {};
+	
+	public FlyingVehicle(short wings, Engine engine) {
+		this.wings = wings;
+		this.engine = engine;
+	}
+	
+	public short getWings() {
+		return wings;
+	}
+	public void setWings(short wings) {
+		this.wings = wings;
+	}
+	public Engine getEngine() {
+		return engine;
+	}
+	public void setEngine(Engine engine) {
+		this.engine = engine;
+	}
+	
+	public void fly() {
+		System.out.println("FlyingVehicle is flying...");
+	}
+	public void refuel() {
+		System.out.println("FlyingVehicle is refueling");
+	}
+	public void liftOff() {
+		System.out.println("Flying vehicle is lifting off...");
+	}
+	public void land() {
+		System.out.println("FlyingVehicle is landing...");
+	}
+}
